5507630d2a473b953d75333b5417a346c143871c
REVA CONSULTANTS LLP <goutam@revateam.com>
2023-02-08 16:54:00 +0530

    new commit on 08-02

--- app/src/main/java/com/erspl/myapplicationas/MainActivity2.java
+++ app/src/main/java/com/erspl/myapplicationas/MainActivity2.java
@@ -3,14 +3,12 @@ package com.erspl.myapplicationas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"Dsd",Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main2);
     }
 }
